0602f7f6d2c13eb17fb956cfec6650f981300f2e
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2023-12-17 13:03:17 +0530

    Complete playRound function for the functionality of the game

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ function getComputerChoice() {
 
 const computerChoice = getComputerChoice();
 console.log(computerChoice);
+
+const playerChoice = prompt("Enter a choice: ").toLowerCase();
+console.log(playerChoice);
+
+// Function for playing the game
+function playRound(playerChoice, computerChoice) {
+  // THE COMPUTER CHOOSES ROCK
+  if (playerChoice === "rock" && computerChoice === "Rock") {
+    return "It's a tie.";
+  } else if (playerChoice === "scissors" && computerChoice === "Rock") {
+    return "You lose! The computer chose rock.";
+  } else if (playerChoice === "paper" && computerChoice === "Rock") {
+    return "You win! The computer chose rock.";
+  }
+  // THE COMPUTER CHOOSES PAPER
+  else if (playerChoice === "rock" && computerChoice === "Paper") {
+    return "You lose! The computer chose paper.";
+  } else if (playerChoice === "paper" && computerChoice === "Paper") {
+    return "It's a tie.";
+  } else if (playerChoice === "scissors" && computerChoice === "Paper") {
+    return "You win! The computer chose paper.";
+  }
+  // THE COMPUTER CHOSE SCISSORS
+  else if (playerChoice === "rock" && computerChoice === "Scissors") {
+    return "You win! The computer chose scissors.";
+  } else if (playerChoice === "paper" && computerChoice === "Scissors") {
+    return "You lose! The computer chose scissors.";
+  } else if (playerChoice === "scissors" && computerChoice === "Scissors") {
+    return "It's a tie.";
+  } else {
+    return "Please enter a valid choice.";
+  }
+}
+
+console.log(playRound(playerChoice, computerChoice));

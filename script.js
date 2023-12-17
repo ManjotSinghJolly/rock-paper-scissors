e96e5b5c80cdddf63617bda1c444b15bae7b6e43
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2023-12-17 12:08:19 +0530

    Complete getComputerChoice function for generating random computer choice

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("Hello, World!");
-console.log("The JavaScript file is working!");
+// console.log("Hello, World!");
+// console.log("The JavaScript file is working!");
+
+// Generates a random whole number between 1 and 3
+function getComputerChoice() {
+  let x = Math.floor(Math.random() * 3 + 1);
+  console.log(x);
+
+  if (x == 1) {
+    return "Rock";
+  } else if (x == 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
